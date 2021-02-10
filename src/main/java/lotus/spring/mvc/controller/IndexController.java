ce5a44cff942bc1924bed68268b086e46a7ce7c9
lotus <wowkaya@gmail.com>
2021-02-10 15:37:49 +0900

    첫번째 커밋

---
+++ src/main/java/lotus/spring/mvc/controller/IndexController.java
@@ -0,0 +1,14 @@
+package lotus.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
